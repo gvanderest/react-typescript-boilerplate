ac901a66569096f692e29c70f83dc88fc493a6d5
Guillaume VanderEst <guillaume@vanderest.org>
2017-12-02 13:41:59 -0800

    feat(webpack): add hash to html plugin for cache busting

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: "src/static/index.html",
+            hash: true,
             inject: "body"
         }),
         new UnusedFilesWebpackPlugin({
