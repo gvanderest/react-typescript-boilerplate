05867a93e6f8d74468fade0707ebb634ee16b398
Guillaume VanderEst <guillaume@vanderest.org>
2018-01-25 20:01:30 -0800

    feat(application): add timer for application testing

--- src/application.tsx
+++ src/application.tsx
@@ -9,16 +9,45 @@ interface IProps {
     text: string;
 }
 
-class Example extends React.Component<IProps> {
+interface IState {
+    count: number;
+    tickIntervalId: number | undefined;
+}
+
+class Example extends React.PureComponent<IProps, IState> {
+    public constructor(props: IProps) {
+        super(props);
+        this.state = {
+            count: 0,
+            tickIntervalId: undefined,
+        };
+    }
+    public componentDidMount(): void {
+        const ONE_SECOND: number = 1000;
+        this.setState({
+            tickIntervalId: setInterval(this.tick.bind(this), ONE_SECOND),
+        });
+    }
+    public componentWillUnmount(): void {
+        clearInterval(this.state.tickIntervalId);
+    }
     public render(): JSX.Element {
         const { text } = this.props;
+        const { count } = this.state;
 
         return (
             <div>
                 <p><img src={ exodusLogo } /></p>
-                <h1>Example Application: { text }</h1>
+                <h1>Example Application: { text } - { count }</h1>
             </div>
         );
+    }
+    protected tick(): void {
+        const { count } = this.state;
+
+        this.setState({
+            count: count + 1,
+        });
     }
 }
 
