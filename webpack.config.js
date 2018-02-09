b2541b45484662f4b4d18ea51309820304d65cd7
Guillaume VanderEst <guillaume@vanderest.org>
2018-02-09 13:23:18 -0800

    feat(webpack): change apostrophes to quotes and add optimizations

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 /*global module, require, __dirname*/
-var path = require('path');
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require("path");
+var webpack = require("webpack");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 var UnusedFilesWebpackPlugin = require("unused-files-webpack-plugin").default;
 var figlet = require("figlet");
-var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-var CleanWebpackPlugin = require('clean-webpack-plugin');
+var UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+var BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+var CleanWebpackPlugin = require("clean-webpack-plugin");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 
@@ -24,7 +24,7 @@ module.exports = function(env) {
 
     var extractStyles = new ExtractTextPlugin("styles.css");
     var cssLoader = {
-        loader: 'css-loader',
+        loader: "css-loader",
         options: {
             minimize: production
         }
@@ -38,14 +38,14 @@ module.exports = function(env) {
             inject: "body"
         }),
         new UnusedFilesWebpackPlugin({
-            patterns: SOURCE_FOLDER + '/**/*.*'
+            patterns: SOURCE_FOLDER + "/**/*.*"
         }),
         extractStyles
     ];
 
     if (production) {
         plugins.push(new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('production')
+            "process.env.NODE_ENV": JSON.stringify("production")
         }));
         plugins.push(new UglifyJSPlugin({
             sourceMap: true,
@@ -59,30 +59,60 @@ module.exports = function(env) {
         }));
     }
 
+    var expireDate = new Date(new Date() + (365 * 24 * 60 * 60 * 1000));
+    console.log(expireDate);
+
     return {
-        entry: './' + SOURCE_FOLDER + '/application.tsx',
-        devtool: 'source-map',
+        entry: "./" + SOURCE_FOLDER + "/application.tsx",
+        devtool: "source-map",
         output: {
             path: path.resolve(__dirname, RELEASE_FOLDER),
-            filename: 'bundle.js'
+            filename: "bundle.js"
         },
         resolve: {
             extensions: [
-                '.js', '.jsx',
-                '.ts', '.tsx',
-                '.css', '.styl', '.less',
-                '.gif', '.png', '.jpeg', '.jpg', '.svg'
+                ".js", ".jsx",
+                ".ts", ".tsx",
+                ".css", ".styl", ".less",
+                ".gif", ".png", ".jpeg", ".jpg", ".svg"
             ]
         },
         module: {
             rules: [
-                { test: /\.tsx?$/i, loader: 'awesome-typescript-loader' },
+                { test: /\.tsx?$/i, loader: "awesome-typescript-loader" },
                 { test: /\.css$/i, use: extractStyles.extract([cssLoader]) },
-                { test: /\.styl$/i, use: extractStyles.extract([cssLoader, 'stylus-loader']) },
-                { test: /\.(gif|png|jpe?g|svg)$/i, use: 'file-loader' }
+                { test: /\.styl$/i, use: extractStyles.extract([cssLoader, "stylus-loader"]) },
+                { test: /\.(gif|png|jpe?g|svg)$/i, use: [
+                    "file-loader",
+                    {
+                        loader: "image-webpack-loader",
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65
+                            },
+                            // optipng.enabled: false will disable optipng
+                            optipng: {
+                                enabled: false,
+                            },
+                            pngquant: {
+                                quality: "65-90",
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            // the webp option will enable WEBP
+                            webp: {
+                                quality: 75
+                            }
+                        }
+                    }
+                ] }
             ]
         },
         devServer: {
+            compress: true,
             historyApiFallback: true
         },
         plugins: plugins
